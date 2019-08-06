fa265890bfff95d67368268baea7f00b1e9831f1
James Browning <JamesB.fe80@gmail.com>
2019-08-06 08:07:31 -0700

    Add many URL derived changes and one time based change

--- driver_nmea0183.c
+++ driver_nmea0183.c
@@ -288,7 +288,7 @@ static void register_fractional_time(const char *tag, const char *fld,
  *                                see faa_mode() for possible mode values
  *
  * see also:
- * https://gpsd.gitlab.io/gpsd/NMEA.html#_vtg_track_made_good_and_ground_speed
+ * https://gpsd.io/NMEA.html#_vtg_track_made_good_and_ground_speed
  */
 static gps_mask_t processVTG(int count,
                              char *field[],
