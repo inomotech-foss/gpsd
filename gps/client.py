d636ceffce9ddd5b687fbbd3cdf1ec4dcadf8ef5
beta-tester <6250964-beta-tester@users.noreply.gitlab.com>
2022-07-11 06:43:03 +0000

    fix: returning same data item from next()

--- gps/client.py
+++ gps/client.py
@@ -145,7 +145,7 @@ class gpscommon(object):
                 frag = self.sock.recv(8192)
 
             self.linebuffer += frag
-            if not self.linebuffer:
+            if not frag or not self.linebuffer:
                 if self.verbose > 1:
                     sys.stderr.write(
                         "poll: no available data: returning -1.\n")
@@ -158,7 +158,7 @@ class gpscommon(object):
                     sys.stderr.write("poll: partial message: returning 0.\n")
                 # Read succeeded, but only got a fragment
                 self.response = ''  # Don't duplicate last response
-                self.bresponse = ''  # Don't duplicate last response
+                self.bresponse = b''  # Don't duplicate last response
                 return 0
         else:
             if self.verbose > 1:
